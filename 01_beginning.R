5b3d5c6a840a9f69246dba67f39859d1213230cc
sofiyatumanova <146854654+sofiyatumanova@users.noreply.github.com>
2023-10-03 17:52:24 +0200

    Update 01_beginning.R

--- 01_beginning.R
+++ 01_beginning.R
@@ -15,4 +15,25 @@ final
 
 final^2
 
+# array
+
+sophi <- c(10,20,30,50,70) #microplastics 
+#functions have parentheses and inside them are arguments
+
+paula <- c(100,500,600,1000,2000) #people
+
+plot(paula,sophi)
+
+plot(paula,sophi,xlab="number of people",ylab="microplastics")
+
+people <- paula
+microplastics <- sophi
+
+plot(people, microplastics) 
+
+plot(people,microplastics, pch=19) #pch changes how the point looks like, you can fin this info online
+plot(people,microplastics, pch=19, cex=2) #cex is the diameter of the point symbol
+plot(people,microplastics, pch=19, cex=2, col="blue") #col is to change the color
+
+
 
