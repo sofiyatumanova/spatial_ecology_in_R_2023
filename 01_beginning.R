e07f70c25cb3d8878134ad92a614f36d764fe921
sofiyatumanova <146854654+sofiyatumanova@users.noreply.github.com>
2023-10-03 17:25:39 +0200

    Create 01_beginning.R

---
+++ 01_beginning.R
@@ -0,0 +1,18 @@
+# it is important, for the exam that every operation that you comment what you are doing
+
+# R as a calculator
+2+3
+
+# Assign to an object
+dima <- 2+3 
+dima
+
+duccio <- 5+3
+duccio
+
+final <- dima * duccio
+final
+
+final^2
+
+
