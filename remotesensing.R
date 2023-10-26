327cb7562fa0b42001d2208a2907f4bb5284ec19
sofiyatumanova <146854654+sofiyatumanova@users.noreply.github.com>
2023-10-26 17:04:34 +0200

    Update remotesensing.R

--- remotesensing.R
+++ remotesensing.R
@@ -11,5 +11,11 @@ devtools::install_github("ducciorocchini/imageRy")
 install.packages("imageRy")
 library(imagery)
 
+# listing the data | gives you the list of all the files that are available in the imageRy package
 im.list()
+# importing the data image
 b2 <- im.import("sentinel.dolomites.b2.tif") #we are importing the band 2 of Sentinel 2 (blue band)
+b2 # we see the information available about this band/image
+
+
+
