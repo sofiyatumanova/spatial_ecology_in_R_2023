b19f68aaf70313735b65b3783b6f99038747b472
sofiyatumanova <146854654+sofiyatumanova@users.noreply.github.com>
2023-11-14 17:43:02 +0100

    Update 06.time.series.R

--- 06.time.series.R
+++ 06.time.series.R
@@ -24,3 +24,37 @@ plot(dif)
 #palette
 cldif <- colorRampPalette(c("blue","white","red")) (100) # red parts mean NO2 was higher in January, blue parts men that NO2 was higher in March
 plot(dif, col = cldif)
+
+### New example : temperature in Greenland
+
+g2000 <- im.import("greenland.2000.tif") # greenland in 2000
+# changing the color ramp
+clg <- colorRampPalette(c("black","blue","white","red")) (100)
+plot(g2000, col= clg)
+
+g2005 <- im.import("greenland.2005.tif")
+g2010 <- im.import("greenland.2010.tif")
+g2015 <- im.import("greenland.2015.tif")
+
+plot(g2015, col= clg)
+
+# plotting them together
+par(mfrow= c(1,2))
+plot(g2000, col= clg) # wider black part means more ice extent in greenland
+plot(g2015, col= clg) # we see that in 2015 the ice extent is much smaller
+
+# stacking the images together
+stackg <- c(g2000, g2005, g2010, g2015)
+plot(stack, col = clg)
+
+# exersice : make difference between teh first and final elements of the stack
+gdif <- g2000 - g2015
+dev.off
+plot(gdif)
+
+# another way
+gdif <- stackg[[1]] - stackg[[4]]
+plot(gdif, col = clg)
+
+# exercise : make an RGB plot using different years
+im.plotRGB(stackg, r = 1, g = 2, b = 3) # using the different elements of a stack to create the RGB
