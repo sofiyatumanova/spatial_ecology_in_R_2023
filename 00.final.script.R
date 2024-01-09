8fc14b7b654a7a6f5912ab5702e40016096a1dab
sofiyatumanova <146854654+sofiyatumanova@users.noreply.github.com>
2024-01-09 17:07:12 +0100

    Update 00.final.script.R

--- 00.final.script.R
+++ 00.final.script.R
@@ -5,6 +5,9 @@
 # 02.1 Population Density
 # 02.2 Population Distribution
 # 03 Communities Multivariate Analysis
+# 04 Remote Sensing Visualization
+# 05 Spectral Indices
+# 06 Time Series
 
 #---------------
 
@@ -270,4 +273,247 @@ plot(ord) # we can see from the plot that some species like to stay together (cl
 
 #---------------
 
+# 04 Remote Sensing Visualization
 
+###  November 2 2023
+## Code to visualize remote sensing data
+
+library(devtools) # packages in R are also called libraries
+
+# install the imageRy package from GitHub
+devtools::install_github("ducciorocchini/imageRy")
+
+library(imageRy)
+library(terra)
+
+# simple operation
+10 + 10
+
+# this is an object
+duccio <- 10 + 10
+duccio
+
+# objects
+adam <- 5 + 3
+duccio + adam
+
+# arrays
+diameter <- c(100, 10, 50, 20, 15)
+iron <- c(10, 1000, 20, 700, 900)
+
+# a function
+plot(iron, diameter) # iron and diameter are arguments!
+plot(iron, diameter, pch=19) # https://www.google.com/search?client=ubuntu-sn&hs=Ssn&sca_esv=564367827&channel=fs&sxsrf=AB5stBhOTkEGpHkLRMvuoUQuTCdqYjKtEw:1694448980289&q=pch+in+R&tbm=isch&source=lnms&sa=X&ved=2ahUKEwjzmr2s-qKBAxUOGuwKHUdlAnMQ0pQJegQIDBAB&biw=960&bih=484&dpr=2#imgrc=lUw3nrgRKV8ynM
+plot(iron, diameter, pch=19, cex=2)
+plot(iron, diameter, pch=19, cex=2, col="red")
+
+# do you want info about functions?
+?plot()
+
+# now we will use a package
+        # gives us a list of all the data
+im.list()    
+
+        # importing the bands
+b2 <- im.import("sentinel.dolomites.b2.tif")
+
+        # import the green band from Sentinel-2 (band 3)
+b3 <- im.import("sentinel.dolomites.b3.tif") 
+plot(b3, col=cl)
+
+        # import the red band from Sentinel-2 (band 4)
+b4 <- im.import("sentinel.dolomites.b4.tif") 
+plot(b4, col=cl)
+
+        # import the NIR band from Sentinel-2 (band 8)
+b8 <- im.import("sentinel.dolomites.b8.tif") 
+plot(b8, col=cl)
+
+        # multiframe
+par(mfrow=c(2,2))
+plot(b2, col=cl)
+plot(b3, col=cl)
+plot(b4, col=cl)
+plot(b8, col=cl)
+
+        # stack images | you can but the bands as elements of an array that defines an object
+stacksent <- c(b2, b3, b4, b8)
+stacksent
+
+dev.off() # it closes devices
+plot(stacksent, col=cl)
+        # plotting the 4th element of the stack (nir band) only 
+plot(stacksent[[4]], col=cl)
+
+
+# Exercise: plot in a multiframe the bands with different color ramps
+par(mfrow=c(2,2))
+
+clb <- colorRampPalette(c("dark blue", "blue", "light blue")) (100)
+plot(b2, col=clb)
+
+clg <- colorRampPalette(c("dark green", "green", "light green")) (100)
+plot(b3, col=clg)
+
+clr <- colorRampPalette(c("dark red", "red", "pink")) (100)
+plot(b4, col=clr)
+
+cln <- colorRampPalette(c("brown", "orange", "yellow")) (100)
+plot(b8, col=cln)
+
+
+# RGB space
+# stacksent: 
+# band2 blue element 1, stacksent[[1]] 
+# band3 green element 2, stacksent[[2]]
+# band4 red element 3, stacksent[[3]]
+# band8 nir element 4, stacksent[[4]]
+im.plotRGB(stacksent, r=3, g=2, b=1) # true color composite
+im.plotRGB(stacksent, r=4, g=3, b=2)
+im.plotRGB(stacksent, r=3, g=4, b=2)
+im.plotRGB(stacksent, r=3, g=2, b=4)
+
+
+pairs(stacksent)
+
+#---------------
+
+# 05 Spectral Indices
+
+### November 7 2023
+
+# vegetation indices
+
+library(imageRy)
+library(terra)
+
+im.list()
+
+m1992 <- im.import("matogrosso_l5_1992219_lrg.jpg")   
+# bands: 1=NIR, 2=RED, 3=GREEN
+im.plotRGB(m1992, r=1, g=2, b=3)
+im.plotRGB(m1992, 1, 2, 3)
+im.plotRGB(m1992, r=2, g=1, b=3) # we put the NIR band on the green
+im.plotRGB(m1992, r=2, g=3, b=1) # we put the NIR band on the blue, everything that's bare soil will show up as yellow
+
+
+# import the recent image
+m2006 <- im.import("matogrosso_ast_2006209_lrg.jpg")
+im.plotRGB(m2006, r=2, g=3, b=1)
+
+# Excersise : build the multiframe with the two images from 1992 and 2006
+par(mfrow=c(1,2)) # one row and two columns
+im.plotRGB(m1992, r=1, g=2, b=3)
+im.plotRGB(m2006, r=1, g=2, b=3)
+
+
+dev.off()
+plot(m1992[[1]]) # plotting only the first element
+
+# a "bit" is either 0 or 1, it is a binary code
+# 2^3 = 8 bit
+# 2^4 = 16 bit
+# 2^8 = 256   # most images are 8 bit
+
+
+# DVI = NIR - R
+# NIR = element 1
+# RED = element 2
+
+# DVI of 1992 is an operation so we use the "=" not the "<-"
+dvi1992 = m1992[[1]] - m1992[[2]]
+plot(dvi1992) # anything from 0 to 200 should be healthy, below 0 to -200 should be sufferng vegetation
+# changing the color
+cl <- colorRampPalette(c("dark blue", "yellow", "red", "black")) (100)
+plot(dvi1992, col = cl) # everything which is dark red is healthy vegetation, going towards yellow and blue means unhealthy vegetation/ bare soil
+
+
+# Exercise : calculate dvi of 2006 image
+dvi2006 = m2006[[1]] - m2006[[2]]
+plot(dvi2006)
+cl <- colorRampPalette(c("dark blue", "yellow", "red", "black")) (100)
+plot(dvi2006, col = cl)
+
+# NDVI : Normalized Difference Vegetation Index | always ranges from -1 to 1
+
+ndvi1992 = dvi1992 / (m1992[[1]] + m1992[[2]])  # (NIR-R)/(NIR+R)
+plot(ndvi1992, col = cl) # the new range is exactly -1 to 1, therefore it can be compared with other images unlike DVI which has a different range
+# or ndvi1992 = (m1992[[1]] - m1992[[2]])/ (m1992[[1]] + m1992[[2]])
+
+# NDVI for 2006 image
+ndvi2006 = dvi2006 / (m2006[[1]] + m2006[[2]])  # (NIR-R)/(NIR+R)
+plot(ndvi2006, col = cl)
+
+par(mfrow=c(1,2)) # one row and two columns
+plot(ndvi1992, col= cl)
+plot(m2006, col = cl)
+
+
+# speeding up the calculation
+ndvi2006a <- im.ndvi(m2006, 1, 2) # function to calculate NDVI automatically is "im.ndvi" | 1 = NIR band , 2 = RED band
+plot(ndvi2006a, col = cl)
+
+#---------------
+
+# 06 Time Series
+
+#### Time Series Analysis | November 14, 2023
+
+library(imageRy)
+library(terra)
+
+# importing data:
+im.list()
+
+EN01 <- im.import("EN_01.png") # shows the levels of Nitrogen (NO2) across Europe in January
+EN13 <- im.import("EN_13.png") # amount of Nitrogen (NO2) in March
+
+par(mfrow= c(2,1))
+im.plotRGB.auto(EN01)
+im.plotRGB.auto(EN13)
+
+# seeing the difference between the two months
+
+dif = EN01[[1]]-EN13[[1]] # using the first band of each of these images, so we put [[]]
+dev.off()
+plot(dif)
+
+# changing the color ramp to see the differences more clearly, pay attention to the colors so everyone is able to interpret them
+
+#palette
+cldif <- colorRampPalette(c("blue","white","red")) (100) # red parts mean NO2 was higher in January, blue parts men that NO2 was higher in March
+plot(dif, col = cldif)
+
+### New example : temperature in Greenland
+
+g2000 <- im.import("greenland.2000.tif") # greenland in 2000
+# changing the color ramp
+clg <- colorRampPalette(c("black","blue","white","red")) (100)
+plot(g2000, col= clg)
+
+g2005 <- im.import("greenland.2005.tif")
+g2010 <- im.import("greenland.2010.tif")
+g2015 <- im.import("greenland.2015.tif")
+
+plot(g2015, col= clg)
+
+# plotting them together
+par(mfrow= c(1,2))
+plot(g2000, col= clg) # wider black part means more ice extent in greenland
+plot(g2015, col= clg) # we see that in 2015 the ice extent is much smaller
+
+# stacking the images together
+stackg <- c(g2000, g2005, g2010, g2015)
+plot(stack, col = clg)
+
+# exersice : make difference between teh first and final elements of the stack
+gdif <- g2000 - g2015
+dev.off
+plot(gdif)
+
+# another way
+gdif <- stackg[[1]] - stackg[[4]]
+plot(gdif, col = clg)
+
+# exercise : make an RGB plot using different years
+im.plotRGB(stackg, r = 1, g = 2, b = 3) # using the different elements of a stack to create the RGB
